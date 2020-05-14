3cc2fa25ff69a7c38b3a4684e2673cf46de6f02f
yulinzhihou <yulinzhihou@gmail.com>
2020-05-14 14:58:23 +0000

    5-14 优化调整数据库配置文件

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
