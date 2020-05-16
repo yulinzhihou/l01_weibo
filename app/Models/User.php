fba5d338366a1a76f391de046f7d993e51503de0
yulinzhihou <yulinzhihou@gmail.com>
2020-05-16 13:15:20 +0000

    粉丝数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use function Sodium\compare;
 
 class User extends Authenticatable
 {
@@ -66,8 +67,63 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    /**
+     * 用户列表博客
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function feed()
     {
         return $this->statuses()->orderBy('created_at','desc');
+    }
+
+    /**
+     * 用户粉丝
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+    /**
+     * 用户关注的人
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+    }
+
+
+    /**
+     * 关注
+     * @param $user_ids  integer|array  用户ID
+     */
+    public function follow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->attach($user_ids,false);
+    }
+
+    /**
+     * 取消关注
+     * @param $user_ids
+     */
+    public function unfollow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    /**
+     * 是否为粉丝，或者关注某人
+     * @param $user_id
+     * @return
+     */
+    public function isFollowers($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
